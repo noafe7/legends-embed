a0a22958ca986887c82563dd96ab0836769e41d2
noafe7 <noafe@wix.com>
2026-06-30 15:57:13 +0300

    Update legend-embed.js

--- legend-embed.js
+++ legend-embed.js
@@ -3,7 +3,7 @@ class LegendEmbed extends HTMLElement {
     this.innerHTML = `
  <div style="height:600px;">
         <iframe
-          src="https://noafe4.wixsite.com/default-dolly-122"
+          src="https://noafe4.wixsite.com/default-dolly-122?configId=3ec59801-b356-4623-bd58-a5928adcb6fc"
           allow="camera; microphone; autoplay"
           style="width:100%; height:100%; border:none;"
         ></iframe>
